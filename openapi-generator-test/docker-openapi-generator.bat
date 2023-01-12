8fd6770a39874f0064d4b88f4e6b3738eb3c45d9
Muil Yang <ymi1210@midasit.com>
2023-01-12 14:16:58 +0900

    api-generator bat 파일 오타 수정.

--- openapi-generator-test/docker-openapi-generator.bat
+++ openapi-generator-test/docker-openapi-generator.bat
@@ -1,1 +1,1 @@
-docker run --rm -v %cd%/apigen-output1:/local openapitools/openapi-generator-cli generate -i http://localhost:8000 -g cpp-restsdk -o /local -p packageName=MuilProject -p apiPackage=muil.api -p modelPackage=muil.model
+docker run --rm -v %cd%/apigen-output:/local openapitools/openapi-generator-cli generate -i http://localhost:8000 -g cpp-restsdk -o /local -p packageName=MuilProject -p apiPackage=muil.api -p modelPackage=muil.model
